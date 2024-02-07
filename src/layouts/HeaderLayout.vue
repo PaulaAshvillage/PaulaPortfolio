e5cb3a10f8c27fcc4668b3cc6c533aabab80b146
Paula Aschendorf <paula.aschendorf@gmail.com>
2024-02-07 11:48:06 +0100

    adds basecss and reset css and home view

---
+++ src/layouts/HeaderLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav>
+      <router-link id="home-link" to="/">paula.</router-link>
+      <router-link class="router-link" to="/projects">projects</router-link>
+      <router-link class="router-link" to="/about">about</router-link>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+<style scoped>
+nav {
+  padding-top: 2rem;
+  padding-right: 4rem;
+  display: flex;
+  justify-content: flex-end;
+  gap: 2rem;
+  font-size: 1.5rem;
+}
+.router-link:hover {
+  text-decoration: underline;
+  text-decoration-color: var(--color-purple);
+  cursor: pointer;
+}
+
+#home-link {
+  font-weight: 100;
+  margin-right: auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,59 @@
-<script setup></script>
-
 <template>
-  <h1>HomeView</h1>
+  <div class="home-container">
+    <h1>Welcome to my portfolio<span id="dot">.</span></h1>
+    <div class="link-container">
+      <router-link id="project-shape" class="shaped-link" to="/projects">projects </router-link>
+      <router-link id="about-shape" class="shaped-link" to="/about">about </router-link>
+    </div>
+  </div>
 </template>
+<style scoped>
+.home-container {
+  height: 100%;
+  padding-inline: 2rem;
+  padding-top: 30vh;
+  display: flex;
+  align-items: flex-end;
+}
+h1 {
+  font-size: 7rem;
+  text-align: left;
+}
+#dot {
+  color: var(--color-purple);
+}
+.link-container {
+  margin-right: 10%;
+  display: flex;
+
+  gap: 2rem;
+  flex-direction: column;
+}
+.shaped-link {
+  font-weight: 600;
+  font-size: 1.75rem;
+}
+#project-shape {
+  color: var(--color-beige);
+  background-color: var(--color-orange);
+  height: 14rem;
+  width: 14rem;
+  border-radius: 14rem 0 0 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: end;
+}
+
+#about-shape {
+  color: var(--color-beige);
+
+  background-color: var(--color-yellow);
+  height: 14rem;
+  width: 14rem;
+  border-radius: 0 14rem 0 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: start;
+}
+</style>
+<script></script>
