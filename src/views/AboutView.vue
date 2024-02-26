29d8f21da96703aaa2d1fea29f3499ed032eb754
Paula Aschendorf <paula.aschendorf@gmail.com>
2024-02-26 12:12:08 +0100

    adds pdf download

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,24 +3,25 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 <template>
   <div>
-    <h1>hallo ich bin paula <span>.</span></h1>
+    <h1>hallo ich bin paula<span>.</span></h1>
 
     <p class="profession">frontend developer | sport scientist</p>
     <article class="about">
       <h2><span>M</span>eine Reise - so far</h2>
       <p>
         Ich habe vor 3 Jahren durch eine glückliche Fügung die Faszination des Codings für mich
-        entdeckt. Meine ersten Schritte mit HTML und CSS führten zu Algorithmen Knobelei in C# bis
-        zu Taschenrechner Frimelei mit Javascript, jQuery und Knockout. Wie umständlich das war,
-        habe ich dann in einem 9-monatigen Coding Bootcamp zur Frontend Developerin erfahren.
-        Außerdem habe ich erfahren, wie viel mehr man in kurzer Zeit lernt, wenn der Pfad vorgegeben
-        und der Stoff didaktisch aufbereitet ist. Ich hatte sehr viel Freude mit barrierfreien HTML
-        und CSS, Knobeleien mit Javascript. Mit Vue.js hat sich dann eine neue Welt eröffnet. Was
-        ich gelernt habe sind die Skills auf der einen Seite, aber vor allem auch den Weg der
-        Problemlösung der damit beginnt ein Problem richtig zu formulieren, sprich ich habe gelernt
-        gut und effektiv zu Googeln. Apropos Google, meinen Einstieg in die Welt der IT begann in
-        einer Firma die eine SEO Software programmiert und vertreibt. SEO ist mir bestens bekannt
-        sowie Lernen, Teamgeist und Empathie.
+        entdeckt. Meine ersten Schritte mit dem Bau einer Landingpage in HTML und CSS führten zu
+        Algorithmen Knobelei in C# hin zu Taschenrechner Frimelei mit Javascript, jQuery und
+        Knockout. Wie umständlich das war, habe ich dann in einem 9-monatigen Coding Bootcamp zur
+        Frontend Developerin erfahren. Außerdem habe ich erfahren, wie viel mehr man in kurzer Zeit
+        lernt, wenn der Pfad vorgegeben und der Stoff didaktisch aufbereitet ist. Barrierfreies HTML
+        und CSS, Knobeleien mit Javascript und die Erstellung von Web Apps mit Vanilla JavaScript.
+        Der Blick ins Backend und das Andocken an APIs und als krönenden Abschluß Vue.js. Ich habe
+        eine steile Lernkurve an Skills, aber vor allem auch den Weg der Problemlösung der damit
+        beginnt ein Problem richtig zu formulieren, sprich ich habe gelernt gut und effektiv zu
+        Googeln. Apropos Google, meinen Einstieg in die Welt der IT begann in einer Firma die eine
+        SEO Software programmiert und vertreibt. SEO ist mir bestens bekannt sowie Lernen, Teamgeist
+        und Empathie.
       </p>
     </article>
     <article>
@@ -44,6 +45,15 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
         <font-awesome-icon id="icon-basketball" :icon="['fas', 'basketball']" />
       </p>
     </article>
+    <article>
+      <h2><Span>C</Span>urriculum Vitae</h2>
+
+      <button id="btn-download">
+        <a href="../assets/PaulaAschendorf_Lebenslauf.pdf" download target="_blank"
+          ><font-awesome-icon id="download-icon" :icon="['fas', 'download']" /> Download
+        </a>
+      </button>
+    </article>
   </div>
 </template>
 
@@ -51,6 +61,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 div {
   text-align: left;
   padding-top: 5rem;
+  padding-bottom: 1rem;
 }
 h1 {
   font-size: 2rem;
@@ -87,6 +98,18 @@ p {
 #icon-basketball {
   color: var(--color-orange);
 }
+#btn-download {
+  margin-top: 0.5rem;
+  font-size: 1.5rem;
+  border-radius: 12px;
+  padding: 1rem;
+  border: none;
+  background: var(--color-purple);
+  color: var(--color-beige);
+}
+#download-icon {
+  color: var(--color-beige);
+}
 
 @media (max-width: 1000px) {
   h1 {
@@ -95,6 +118,9 @@ p {
   }
 }
 @media (max-width: 600px) {
+  div {
+    padding-top: 2rem;
+  }
   h1 {
     font-size: 1rem;
     font-weight: 300;
@@ -113,6 +139,13 @@ p {
   }
   .icon-container {
     flex-wrap: wrap;
+    font-size: 2.8rem;
+  }
+
+  #btn-download {
+    font-size: 0.9rem;
+
+    padding: 0.6rem;
   }
 }
 </style>
